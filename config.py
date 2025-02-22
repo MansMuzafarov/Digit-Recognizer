0dde5053133880611c834ad844d535b6631ccd46
MansMuz <115074326+MansMuz@users.noreply.github.com>
2025-02-22 11:53:09 +0100

    Kaggle updates

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 
 # ====== CONFIGURATION ======
 
-train_model = False  # Set to True if you want to train the model again, otherwise it will load the saved model
+train_model = False        # Set to True if you want to train the model again, otherwise it will load the saved model
+
+train_model_kaggle = False # The same for Kaggle
 
 # ====== Parameters for training ======
 
--- kaggle_solution.py
+++ kaggle_solution.py
@@ -4,7 +4,7 @@ import pandas as pd
 from save_load import save_model, load_model
 from data_preprocessing import data_preprocessing_kaggle
 from evaluation import predict
-from config import train_model, alpha, number_of_epochs, batch_size
+from config import train_model_kaggle, alpha, number_of_epochs, batch_size
 from train import train_neural_network
 
 
@@ -19,7 +19,7 @@ def make_predictions_for_kaggle():
     model_path = "trained_model_kaggle.npz"
 
     # ====== Model training or loading ======
-    if train_model or not os.path.exists(model_path):
+    if train_model_kaggle or not os.path.exists(model_path):
         print("\n🔵 Training the model... (Kaggle)\n")
         W_1, b_1, W_2, b_2, loss_history = train_neural_network(
             alpha=alpha, batch_size=batch_size, Number_of_epochs=number_of_epochs, X_train=X_train, Y_train=Y_train
@@ -31,7 +31,7 @@ def make_predictions_for_kaggle():
         loss_history = []  # No loss history if loading a pre-trained model
 
     # ====== Plot loss function history (only if trained) ======
-    if train_model and loss_history:
+    if train_model_kaggle and loss_history:
         plt.plot(loss_history)
         plt.xlabel("Epoch")
         plt.ylabel("Loss function value")
